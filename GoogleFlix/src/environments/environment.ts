b7fc8470a440fa997acaa6cdb447af1678d8378c
Ongun <info@ongunakay.com>
2024-12-27 02:45:06 +0300

    /

--- GoogleFlix/src/environments/environment.ts
+++ GoogleFlix/src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api' // Example API URL, change as needed
+  apiUrl: 'http://localhost:3000/api', // Example API URL, change as needed
+  firebaseConfig: {
+    apiKey: "AIzaSyDUN24OIuoC6IDvzmjMVJ_DG7RaV5auSJw",
+    authDomain: "gpt-500ef.firebaseapp.com",
+    projectId: "gpt-500ef",
+    storageBucket: "gpt-500ef.firebasestorage.app",
+    messagingSenderId: "631149878533",
+    appId: "1:631149878533:web:07b355e14ddfdac026eb6c",
+    measurementId: "G-JFMZKSDEE7"
+  }
 };
