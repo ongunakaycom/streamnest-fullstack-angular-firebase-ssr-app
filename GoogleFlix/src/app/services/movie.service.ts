e23d38f5ebd3a033f3d99355f5b7308c1feeb651
Ongun <info@ongunakay.com>
2025-01-09 20:10:55 +0300

    //

--- GoogleFlix/src/app/services/movie.service.ts
+++ GoogleFlix/src/app/services/movie.service.ts
@@ -3,17 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
-
-  // This URL should point to your Go backend endpoint
-  private apiUrl = 'http://localhost:8081/movies'; // Go backend URL
+  private apiUrl = 'http://localhost:8081/movies'; // Replace with your Go server URL
 
   constructor(private http: HttpClient) {}
 
-  // Method to get movies by title
-  getMovies(title: string): Observable<any> {
+  getMovie(title: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}?title=${title}`);
   }
 }
