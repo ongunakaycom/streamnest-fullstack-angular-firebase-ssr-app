4510f1067c2a5031e00b982d884edefc5b52d409
Ongun <info@ongunakay.com>
2025-01-09 19:54:34 +0300

    //

---
+++ GoogleFlix/src/app/models/movie.model.ts
@@ -0,0 +1,11 @@
+// movie.model.ts
+export interface Movie {
+    Title: string;
+    Year: string;
+    Genre: string;
+    Director: string;
+    Actors: string;
+    Plot: string;
+    Poster: string;
+  }
+  
