7fdf95abebe4d72989f6563c1fc25e625aa850ec
Ongun <info@ongunakay.com>
2025-01-10 19:54:22 +0300

    //

--- GoogleFlix/src/app/movie.service.ts
+++ GoogleFlix/src/app/movie.service.ts
@@ -6,8 +6,6 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class MovieService {
-  private baseUrl = 'https://google-flix-backend.vercel.app/api/movies';
-
   constructor(private http: HttpClient) {}
 
   getMovies(query: string): Observable<any> {
